e7cb594176a86873d4e92c4fdd605d69c5f9ec50
david <david@static-disco.fak>
2017-12-17 23:13:16 -0600

    Added License

--- CommandFunctions.h
+++ CommandFunctions.h
@@ -1,9 +1,22 @@
 /*
- * CommandFunctions.h
- *
- *  Created on: Dec 3, 2016
- *      Author: david
- */
+
+Robot Main Brain  --  runs on 1284P and handles onboard control of my robot
+     Copyright (C) 2017  David C.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+     */
 
 #ifndef COMMANDFUNCTIONS_H_
 #define COMMANDFUNCTIONS_H_
